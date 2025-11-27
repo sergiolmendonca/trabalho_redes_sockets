91df826671e82259e294167f41bac23e0fee5a48
sergiolmendonca <sergiopromail@gmail.com>
2025-11-27 19:12:33 +0000

    comando pra liberar a interface

--- clienteInterface/Program.cs
+++ clienteInterface/Program.cs
@@ -13,6 +13,7 @@ class Program
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
+    //xhost +local:docker
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
